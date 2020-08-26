eb0c6a0d3e03368f52d8238ef55010c323a0e501
Leonardo <srleon19@hotmail.com>
2020-08-25 22:35:46 -0300

    Spring Data Aula 2

--- tarefas/src/main/java/br/com/fiap/tarefas/domain/Tarefa.java
+++ tarefas/src/main/java/br/com/fiap/tarefas/domain/Tarefa.java
@@ -1,9 +1,35 @@
 package br.com.fiap.tarefas.domain;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@SequenceGenerator(name = "tarefa", sequenceName = "SQ_TAREFA")
 public class Tarefa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tarefa")
+    private long codigo;
+
+    private LocalDate dataLimite;
     private String descricao;
     private boolean feita;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
 
     public String getDescricao() {
         return descricao;
