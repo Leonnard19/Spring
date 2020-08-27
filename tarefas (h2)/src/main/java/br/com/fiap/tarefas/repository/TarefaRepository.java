7166c057fb8e1022acdf7fa9f32e2062352443f0
Leonardo <srleon19@hotmail.com>
2020-08-27 17:03:22 -0300

    Included Methods Put/Delete

--- tarefas (h2)/src/main/java/br/com/fiap/tarefas/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.tarefas.repository;
-
-import br.com.fiap.tarefas.domain.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
